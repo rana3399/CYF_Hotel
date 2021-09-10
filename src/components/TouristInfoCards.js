3cd2e82974e927c6d7fb99a9219ff3172d8c706e
Rana Ahmed <foster3399@gmail.com>
2021-09-11 01:31:48 +0200

    First card created

--- src/components/TouristInfoCards.js
+++ src/components/TouristInfoCards.js
@@ -1,5 +1,44 @@
 import React from "react";
 
 const TouristInfoCards = () => {
-  return <div>Hi</div>;
+  return (
+    <div className="card-img-top">
+      <img
+        className="card-Img"
+        src="https://image.flaticon.com/icons/svg/139/139899.svg"
+      />
+      <div className="card-body">
+        <div className="card-title">
+          <h2>Rome</h2>
+          <div className="card-descriptions">
+            <p>
+              xplore Rome with a 24-, 48-, or 72-hour valid ticket on this
+              open-top, hop-on and hop-off sightseeing bus. Enjoy engaging
+              commentary and see sites such as the Vatican, Colosseum, and
+              Spanish Steps.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
+
+export default TouristInfoCards;
+
+{
+  /* <div className = "Card-img">
+<div className = "Card-title"> This is card title.
+ <div className = "Card-descriptions">
+     <div className = "Card-descriptions">
+         <div className = "Card-info-btn">
+             
+         </div>
+     </div>
+
+ </div>
+
+</div>
+
+</div> */
+}
