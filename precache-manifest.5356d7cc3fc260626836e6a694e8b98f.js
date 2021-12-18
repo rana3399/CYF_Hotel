6293fcfa0bb5634b0859e5ac7024dd26a7d38ecd
Rana_Ahmed <foster3399@gmail.com>
2021-12-19 00:14:25 +0100

    Updates

---
+++ precache-manifest.5356d7cc3fc260626836e6a694e8b98f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b7c209104533a02bc162c231e4a53d6",
+    "url": "/cyf-hotel-react/index.html"
+  },
+  {
+    "revision": "9e0dbcdb486a004515b6",
+    "url": "/cyf-hotel-react/static/css/main.2b9371e4.chunk.css"
+  },
+  {
+    "revision": "5c5831c2056cac893362",
+    "url": "/cyf-hotel-react/static/js/2.04498cb5.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/cyf-hotel-react/static/js/2.04498cb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e0dbcdb486a004515b6",
+    "url": "/cyf-hotel-react/static/js/main.07c83c80.chunk.js"
+  },
+  {
+    "revision": "a17eedfa60f26189a33f",
+    "url": "/cyf-hotel-react/static/js/runtime-main.72cf73f6.js"
+  }
+]);
